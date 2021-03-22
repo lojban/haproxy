2ae583881630c20d46dbc83f8258450c30299f42
Robin Lee Powell <robinleepowell@gmail.com>
2021-03-21 22:12:48 -0700

    Cert renewal: now with more actually working!

--- init_certs.sh
+++ init_certs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-podman exec -u root -it web certbot certonly --standalone \
+podman exec -u root -it web certbot certonly --expand --standalone \
     -d lojban.org \
     -d mw.lojban.org -d mw-live.lojban.org -d mw-test.lojban.org \
     -d jbotcan.org -d jbovlaste.lojban.org -d www.lojban.org \
+    -d vlasisku.lojban.org -d tiki.lojban.org -d test-vs.lojban.org \
+    -d camxes.lojban.org \
     --non-interactive --agree-tos --email webmaster@lojban.org --http-01-port=8888
 podman exec -u root -it web chown -R $(id -un):$(id -gn) /etc/letsencrypt/
 
--- renew_certs.sh
+++ renew_certs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# This is often called from cron, so:
+export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH
+
 podman exec -u root -it web certbot renew --tls-sni-01-port=8888
 podman exec -u root -it web chown -R $(id -un):$(id -gn) /etc/letsencrypt/
 
-cat containers/web/data/letsencrypt/live/lojban.org/fullchain.pem containers/web/data/letsencrypt/live/lojban.org/privkey.pem > containers/web/data/letsencrypt/live/lojban.org/haproxy.pem
+cat /home/sphaproxy/haproxy/containers/web/data/letsencrypt/live/lojban.org/fullchain.pem \
+    /home/sphaproxy/haproxy/containers/web/data/letsencrypt/live/lojban.org/privkey.pem > \
+        /home/sphaproxy/haproxy/containers/web/data/letsencrypt/live/lojban.org/haproxy.pem
 
 systemctl --user restart web
-
