20c8fdb3861424054437d8a7f9700cfeb2bf0072
Robin Lee Powell <robinleepowell@gmail.com>
2021-07-03 22:15:27 -0700

    Added the corpus site.

--- init_certs.sh
+++ init_certs.sh
@@ -5,7 +5,7 @@ podman exec -u root -it web certbot certonly --expand --standalone \
     -d mw.lojban.org -d mw-live.lojban.org -d mw-test.lojban.org \
     -d jbotcan.org -d jbovlaste.lojban.org -d www.lojban.org \
     -d vlasisku.lojban.org -d tiki.lojban.org -d test-vs.lojban.org \
-    -d camxes.lojban.org \
+    -d camxes.lojban.org -d corpus.lojban.org \
     --non-interactive --agree-tos --email webmaster@lojban.org --http-01-port=8888
 podman exec -u root -it web chown -R $(id -un):$(id -gn) /etc/letsencrypt/
 
