c5420a83a66f66cece9a30b9770e32885fc5bfbf
Robin Lee Powell <robinleepowell@gmail.com>
2024-10-14 12:26:18 -0700

    Adding end-of-script line

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Error trapping from https://gist.github.com/oldratlee/902ad9a398affca37bfcfab64612e7d1
+__error_trapper() {
+  local parent_lineno="$1"
+  local code="$2"
+  local commands="$3"
+  echo "error exit status $code, at file $0 on or near line $parent_lineno: $commands"
+}
+trap '__error_trapper "${LINENO}/${BASH_LINENO}" "$?" "$BASH_COMMAND"' ERR
+
+set -euE -o pipefail
+shopt -s failglob
+
 # This is often called from cron, so:
 export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH
 
@@ -11,3 +23,5 @@ cat /home/sphaproxy/haproxy/containers/web/data/letsencrypt/live/lojban.org/full
         /home/sphaproxy/haproxy/containers/web/data/letsencrypt/live/lojban.org/haproxy.pem
 
 systemctl --user restart web
+
+echo "cert renewal complete"
